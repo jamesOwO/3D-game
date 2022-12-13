cf48da3f40746fb4123343cfae11166f186c321b
DESKTOP-CUHP0J3\josh dale <joshuabuzz05@gmail.com>
2022-12-13 15:29:22 +0000

    dskhfb

--- 3D Slime Arena/Assets/Scripts/ThirdPersonMovement.cs
+++ 3D Slime Arena/Assets/Scripts/ThirdPersonMovement.cs
@@ -8,82 +8,97 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ThirdPersonMovement : MonoBehaviour
+namespace UnityEditor.SceneManagement
 {
-    public CharacterController controller;
-    public Transform cam;
 
-    public float speed = 6;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3;
-    Vector3 velocity;
-    bool isGrounded;
 
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundMask;
-
-    Animator anim;
-
-    float turnSmoothVelocity;
-    public float turnSmoothTime = 0.1f;
-
-    void Start()
+    public class ThirdPersonMovement : MonoBehaviour
     {
-        anim = GetComponent<Animator>();
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        //jump
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        public CharacterController controller;
+        public Transform cam;
 
-        if (isGrounded && velocity.y < 0)
+        bool colliding = false;
+        public float speed = 6;
+        public float gravity = -9.81f;
+        public float jumpHeight = 3;
+        Vector3 velocity;
+        bool isGrounded;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+
+        Animator anim;
+
+        float turnSmoothVelocity;
+        public float turnSmoothTime = 0.1f;
+
+        void Start()
         {
-            velocity.y = -2f;
+            anim = GetComponent<Animator>();
         }
-
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);  
-        //walk
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f)
+        // Update is called once per frame
+        void Update()
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y ;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle - 90, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            //jump
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            anim.SetInteger("attack", 1);
-        }
-        else
-        {
-            anim.SetInteger("attack", 0);
-        }
-
-
-    }
-    void OnCollisionStay(Collision collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            bool colliding = true;
-            int animate = anim.GetInteger("attack");
-            if (animate == 1)
+            if (isGrounded && velocity.y < 0)
             {
-                SceneManager.LoadScene(3);
+                velocity.y = -2f;
+            }
+
+            //gravity
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+            //walk
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle - 90, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetInteger("attack", 1);
+                if (colliding == true)
+                {
+                    SceneManager.LoadScene(3);
+                }
+
+            }
+            else
+            {
+                anim.SetInteger("attack", 0);
+            }
+
+
+        }
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "ToArena")
+            {
+                SceneManager.LoadScene(2);
+            }
+            if (collision.gameObject.tag == "Enemy")
+            {
+                colliding = true;
+            }
+        }
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                colliding = false;
             }
         }
     }
-
-    
 }
